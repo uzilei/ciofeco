795ad7360b9f6e34bd3a383619682c27b1fa08e3
uzilei <leichanghao1@gmail.com>
2024-11-14 22:08:25 +0100

    Fixed & adjusted UpAttack, changed dash properties

--- Assets/PlayerControll.cs
+++ Assets/PlayerControll.cs
@@ -20,7 +20,6 @@ public class PlayerControll : MonoBehaviour
     Animator anim;
 
     private bool canDash = true;
-    private bool dashed;
     public static PlayerControll Instance;
 
     private bool isAttacking;
@@ -92,7 +91,14 @@ public class PlayerControll : MonoBehaviour
 
         if (Input.GetKeyDown("k") && !isAttacking && !comboCooldownActive)
         {
-            Attack();
+            if (yAxis > 0)
+            {
+                UpAttack();
+            }
+        else
+            {
+                Attack();
+            }
         }
 
         // Reset attack combo logic, more efficient if in Attack() method, but more reliable here
@@ -146,27 +152,20 @@ public class PlayerControll : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftShift) && canDash && !isAttacking)
         {
             StartCoroutine(Dash());
-            dashed = true;
-        }
-        if (Grounded())
-        {
-            dashed = false;
         }
     }
 
-    IEnumerator Dash() // nael mi dovresti spiegare cosa fa tutta questa roba
+    IEnumerator Dash()
     {
         canDash = false;
         pState = PlayerState.Dashing;
         rb.gravityScale = 0;
         rb.linearVelocity = new Vector2(transform.localScale.x * dashSpeed, 0);
-        //Collision Disability with Attackable Layer(Enemies) during the dash
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Default"), LayerMask.NameToLayer("attackable"), true); 
 
         yield return new WaitForSeconds(dashTime);
         rb.gravityScale = gravity;
         pState = PlayerState.Idle;
-        //Collision recovery after dash
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Default"), LayerMask.NameToLayer("attackable"), false);
 
         yield return new WaitForSeconds(dashCooldown);
@@ -224,10 +223,19 @@ public class PlayerControll : MonoBehaviour
         }
             StartCoroutine(EndAttack());
     }
+    void UpAttack()
+    {
+        isAttacking = true;
+        timeSinceAttack = 0f;
+        comboCooldownActive = true;
+        anim.SetTrigger("UpAttacking");
+        Debug.Log("UpAttack");
+        Hit(UpAttackTransform, UpAttackArea, damage);
+        StartCoroutine(EndAttack());
+    }
 
     IEnumerator EndAttack()
     {
-        Debug.Log("EndAttack()");
         yield return new WaitForSeconds(0.4f); // Cooldown between non-combo attacks
         isAttacking = false;
         pState = PlayerState.Idle;
@@ -235,7 +243,7 @@ public class PlayerControll : MonoBehaviour
 
     private void ResetCombo()
     {
-        Debug.Log("ResetCombo");
+        Debug.Log("Combo Reset");
         attackCount = 0;
         comboCooldownActive = false;
         timeSinceAttack = 0f;
