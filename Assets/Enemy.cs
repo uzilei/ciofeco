e85cdb34d4879daa00945a43cb726c87d888264f
uzilei <leichanghao1@gmail.com>
2024-11-14 12:54:59 +0100

    Added enemy recoil

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,12 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float health;
+    [SerializeField] float recoilLength;
+    [SerializeField] float recoilFactor;
+    [SerializeField] bool isRecoiling = false;
+
+    float recoilTimer;
+    Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -16,9 +30,25 @@ public class Enemy : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        if(isRecoiling)
+        {
+            if(recoilTimer < recoilLength)
+            {
+                recoilTimer += Time.deltaTime;
+            }
+            else
+            {
+                isRecoiling = false;
+                recoilTimer = 0;
+            }
+        }
     }
-    public void EnemyHit(float _damageDone)
+    public void EnemyHit(float _damageDone, Vector2 _hitDirection, float _hitforce)
     {
         health -= _damageDone;
+        if(!isRecoiling)
+        {
+            rb.AddForce(-_hitforce * recoilFactor * _hitDirection);
+        }
     }
 }
