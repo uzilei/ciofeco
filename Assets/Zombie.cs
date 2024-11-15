992e114462a3bedd2068bdfa367b7008f741f655
uzilei <leichanghao1@gmail.com>
2024-11-15 12:58:18 +0100

    Player states and rewrote recoil

--- Assets/Zombie.cs
+++ Assets/Zombie.cs
@@ -3,28 +3,23 @@ using UnityEngine;
 public class Zombie : Enemy
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        rb.gravityScale = 12f;
+    void Start() {
+        rb.gravityScale = 4f;
     }
 
-    protected override void Awake()
-    {
+    protected override void Awake() {
         base.Awake();
     }
 
     // Update is called once per frame
-    protected override void Update()
-    {
+    protected override void Update() {
         base.Update();
-        if (!isRecoiling)
-        {
+        if (!isRecoiling) {
             transform.position = Vector2.MoveTowards(transform.position, new Vector2(PlayerControll.Instance.transform.position.x, transform.position.y), speed * Time.deltaTime);
         }
     }
     
-    public override void EnemyHit(float _damageDone, Vector2 _hitDirection, float _hitforce)
-    {
+    public override void EnemyHit(float _damageDone, Vector2 _hitDirection, float _hitforce) {
         base.EnemyHit(_damageDone, _hitDirection, _hitforce);
     }
 }
